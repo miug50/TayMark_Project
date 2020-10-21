b3e2671eb0dcc889e01f52b8c320e655652e0985
Harry Macinnes <61694770+miug50@users.noreply.github.com>
2020-10-21 14:43:21 +0100

    Add project files.

---
+++ TayMark_Project/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace TayMark_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SideMenuPanel = new System.Windows.Forms.Panel();
+            this.AddEmployees = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panelContainerPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.SideMenuPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panelContainerPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SideMenuPanel
+            // 
+            this.SideMenuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.SideMenuPanel.Controls.Add(this.AddEmployees);
+            this.SideMenuPanel.Controls.Add(this.panel1);
+            this.SideMenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SideMenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.SideMenuPanel.Name = "SideMenuPanel";
+            this.SideMenuPanel.Size = new System.Drawing.Size(250, 597);
+            this.SideMenuPanel.TabIndex = 0;
+            // 
+            // AddEmployees
+            // 
+            this.AddEmployees.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AddEmployees.FlatAppearance.BorderSize = 0;
+            this.AddEmployees.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+            this.AddEmployees.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddEmployees.ForeColor = System.Drawing.Color.White;
+            this.AddEmployees.Location = new System.Drawing.Point(0, 125);
+            this.AddEmployees.Name = "AddEmployees";
+            this.AddEmployees.Size = new System.Drawing.Size(250, 45);
+            this.AddEmployees.TabIndex = 2;
+            this.AddEmployees.Text = "Employees";
+            this.AddEmployees.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AddEmployees.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 125);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(250, 125);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // panelContainerPanel
+            // 
+            this.panelContainerPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.panelContainerPanel.Controls.Add(this.pictureBox1);
+            this.panelContainerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainerPanel.Location = new System.Drawing.Point(250, 0);
+            this.panelContainerPanel.Name = "panelContainerPanel";
+            this.panelContainerPanel.Size = new System.Drawing.Size(986, 597);
+            this.panelContainerPanel.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(223, 125);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(30, 3, 30, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(507, 318);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(1236, 597);
+            this.Controls.Add(this.panelContainerPanel);
+            this.Controls.Add(this.SideMenuPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SideMenuPanel.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panelContainerPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel SideMenuPanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button AddEmployees;
+        private System.Windows.Forms.Panel panelContainerPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ TayMark_Project/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TayMark_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private Form activeForm = null;
+        private void OpenContainerPanel(Form containerForm)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            activeForm = containerForm;
+            containerForm.TopLevel = false;
+            containerForm.FormBorderStyle = FormBorderStyle.None;
+            containerForm.Dock = DockStyle.Fill;
+            panelContainerPanel.Controls.Add(containerForm);
+            panelContainerPanel.Tag = containerForm;
+            containerForm.BringToFront();
+            containerForm.Show();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
